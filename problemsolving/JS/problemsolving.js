f00f45474dd3d6d51a05fc0425fe7040579909c0
Vasantha Rao <navyasril1501@gmail.com>
2024-03-12 11:13:00 +0530

    problemsolving

---
+++ problemsolving/JS/problemsolving.js
@@ -0,0 +1,120 @@
+//find leap year or not?
+// let year = Number(prompt("enter year"));
+// if ((year % 4 == 0 && year % 100 !== 0) || year % 400 == 0) {
+//   console.log("it is a leap year");
+// } else {
+//   console.log("it is not a leap year");
+// }
+//temperature reading
+// let temperature = Number(prompt("enter the number"));
+// if (temperature >= 30) {
+//   console.log("weather is cold");
+// } else if (temperature > 18 && temperature < 29);
+// {
+//   console.log("weather is hot");
+// }
+// vowels
+// let alphabet = prompt("enter alphebet");
+// if (
+//   alphabet == "a " ||
+//   alphabet == "e" ||
+//   alphabet == "i" ||
+//   alphabet == "o" ||
+//   alphabet == "u" ||
+//   alphabet == "A " ||
+//   alphabet == "E" ||
+//   alphabet == "I" ||
+//   alphabet == "O" ||
+//   alphabet == "U"
+// ) {
+//   console.log(alphabet + "'it is a vowel'");
+// } else {
+//   console.log(alphabet + "'it is not a vowel'");
+// }
+// odd or even number
+// let num = Number(prompt("Enter a number"));
+// if (num % 2 === 0) {
+//   console.log("Even number");
+// } else {
+//   console.log("odd number");
+// }
+//week days
+// let Num = Number(prompt("Enter a number "));
+// switch (true) {
+//   case Num === 1:
+//     console.log("Monday");
+//     break;
+//   case Num === 2:
+//     console.log("Tuesday");
+//     break;
+//   case Num === 3:
+//     console.log("Wednesday");
+//     break;
+//   case Num === 4:
+//     console.log("Thursday");
+//     break;
+//   case Num === 5:
+//     console.log("Friday");
+//     break;
+//   case Num === 6:
+//     console.log("Saturday");
+//     break;
+//   case Num === 7:
+//     console.log("Sunday");
+//     break;
+//   default:
+//     console.log("Week have only 7 days ");
+//     break;
+// }
+//months
+// let Num = Number(prompt("Enter month number "));
+// switch (true) {
+//   case Num === 1:
+//     console.log("January");
+//     break;
+//   case Num === 2:
+//     console.log("February");
+//     break;
+//   case Num === 3:
+//     console.log("March");
+//     break;
+//   case Num === 4:
+//     console.log("April");
+//     break;
+//   case Num === 5:
+//     console.log("May");
+//     break;
+//   case Num === 6:
+//     console.log("June");
+//     break;
+//   case Num === 7:
+//     console.log("July");
+//     break;
+//   case Num === 8:
+//     console.log("August");
+//     break;
+//   case Num === 9:
+//     console.log("September");
+//     break;
+//   case Num === 10:
+//     console.log("October");
+//     break;
+//   case Num === 11:
+//     console.log("November");
+//     break;
+//   case Num === 12:
+//     console.log("December");
+//     break;
+//   default:
+//     console.log("Week have only 12 months ");
+//     break;
+// }
+//Equilateral triangle
+let s1 = Number(prompt("Enter 1st side of triangle"));
+let s2 = Number(prompt("Enter 2nd side of triangle"));
+let s3 = Number(prompt("Enter 3rd side of triangle"));
+if (s1 == s2 && s2 == s3 && s1 == s3) {
+  console.log("It is Equilateral triangle");
+} else {
+  console.log("it is not a Equilateral triangle ");
+}
